a773647f0c70ddd3d8b41a0cf2d4c69ef68c4035
leocodesthings <106573277+leocodesthings@users.noreply.github.com>
2023-09-27 18:07:52 +1000

    add rps round results to ui

--- script.js
+++ script.js
@@ -2,28 +2,41 @@ function playRound(playerSelection, computerSelection) {
  //playerSelection = playerSelection.toLowerCase();
     //
     //playerSelection = button.innerText;
+    let gameResults = document.querySelector(".results");
+    let playerScore = document.querySelector(".player");
+    let computerScore = document.querySelector(".computer");
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
         console.log("Rock beats scissors! You won!");
+        gameResults.textContent = "Rock beats scissors! You won!";
+        playerScore.textContent = "player: 1";
 
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         console.log("Scissors cuts paper! You won!");
+        gameResults.textContent = "Scissors cuts paper! You won!";
+
 
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("Paper covers rock! You won!");
+        gameResults.textContent = "Paper covers rock! You won!";
 
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         console.log("You lost! Paper covers rock!");
+        gameResults.textContent = "You lost! Paper covers rock!";
 
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         console.log("You lost! Scissors cuts paper!");
+        gameResults.textContent = "You lost! Scissors cuts paper!";
 
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         console.log("You lost! Rock beats scissors!");
+        gameResults.textContent = "You lost! Rock beats scissors!";
 
     } else if (playerSelection === computerSelection) {
         console.log("Tie!");
+        gameResults.textContent = "Tie!";
     }
+
 }
 // function square(number) {
 //     return number * number;
@@ -46,16 +59,25 @@ function getComputerChoice() {
     }
 }
 
+function game() {
+    for (let i = 0; i === 100; i++) {
+        let playerInput = prompt("Rock, paper, or scissors?");
+        playRound();
+    }
+}
+
 let buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     //console.log(button.innerText);
-    button.addEventListener('click', (event) => {
+    button.addEventListener('click', () => {
         let computerSelection = getComputerChoice();
         playRound(button.innerText, computerSelection);
         console.log(computerSelection);
     });
 });
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+//const playerSelection = "rock";
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
+
+//const rpsChoices = ["rock", "paper", "scissors"]
